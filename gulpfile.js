b44ec5d2f9d6782cdb286248e8ec2a21a54475fa
Martinsito Brito Diaz <martinsitobd@gmail.com>
2022-02-17 11:48:24 -0400

    Add style

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 const { src, dest, watch } = require('gulp');
+const plumber = require('gulp-plumber');
 const sass = require('gulp-sass')(require('sass'));
 
 function css( done ){
     src('src/scss/**/*.scss') //Identificar el archivo css
+        .pipe( plumber() )
         .pipe( sass() )//Compilarlo
         .pipe( dest('build/css'))//Almacenarla en el disco duro
     done();
