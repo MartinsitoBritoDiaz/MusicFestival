b541848368d71f21e76442ffc5ed1c00dc05c186
Martinsito Brito Diaz <martinsitobd@gmail.com>
2022-02-17 11:23:18 -0400

    Add variables

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,14 @@ const { src, dest, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 function css( done ){
-    src('src/scss/app.scss') //Identificar el archivo css
+    src('src/scss/**/*.scss') //Identificar el archivo css
         .pipe( sass() )//Compilarlo
         .pipe( dest('build/css'))//Almacenarla en el disco duro
     done();
 };
 
 function dev( done ){
-    watch('src/scss/app.scss', css)
+    watch('src/scss/**/*.scss', css)
 
     done();
 };
