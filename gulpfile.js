78bd048e8ee379e06bac7cd5bfebd285246d17cc
Martinsito Brito Diaz <martinsitobd@gmail.com>
2022-02-26 15:57:59 -0400

    Creando Galeria de imagenes

--- gulpfile.js
+++ gulpfile.js
@@ -52,15 +52,23 @@ function convertAvif( done ){
     done();
 }
 
+function javascript( done ){
+    src('src/js/**/*.js')
+        .pipe( dest('build/js'));
+
+    done();
+}
 
 function dev( done ){
     watch('src/scss/**/*.scss', css)
+    watch('src/js/**/*.js', javascript)
 
     done();
 };
 
 exports.css = css; 
+exports.js = javascript; 
 exports.images = images; 
 exports.convertWebp = convertWebp;
 exports.convertAvif = convertAvif;
-exports.dev = parallel(images, convertWebp, convertAvif, dev); 
+exports.dev = parallel(images, convertWebp, convertAvif, javascript, dev); 
